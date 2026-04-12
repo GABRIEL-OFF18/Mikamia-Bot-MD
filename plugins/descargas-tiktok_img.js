0e4f2071a34c5f160ee38af3d292167067933f06
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-04-11 19:57:24 -0500

    Actualizar descargas-tiktok_img.js

--- plugins/descargas-tiktok_img.js
+++ plugins/descargas-tiktok_img.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 // --- Constantes y Configuración de Transmisión (Estilo Ellen Joe) ---
-const newsletterJid = '120363418071540900@newsletter';
+const newsletterJid = '120363424677971125@newsletter';
 const newsletterName = '⏤͟͞ू⃪፝͜⁞⟡ 𝐄llen 𝐉ᴏ𝐄\'s 𝐒ervice';
 
 let handler = async (m, { conn, text, args, command, usedPrefix }) => {
