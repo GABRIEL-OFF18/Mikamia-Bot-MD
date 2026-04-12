e94b2fda036b09cb78a778d3406911191a806e4a
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-04-11 19:49:56 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search'
 
 // --- Constantes y Configuración de Transmisión (Estilo Ellen Joe) ---
-const newsletterJid = '120363418071540900@newsletter';
+const newsletterJid = '120363424677971125@newsletter';
 const newsletterName = '⏤͟͞ू⃪፝͜⁞⟡ 𝐄llen 𝐉ᴏ𝐄\'s 𝐒ervice';
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
